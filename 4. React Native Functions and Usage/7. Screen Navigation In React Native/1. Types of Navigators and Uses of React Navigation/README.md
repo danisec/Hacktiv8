3fa4ba622f990cd3352f560e5b2bd25c8815bd40
danisec <daniaprilyanto35@gmail.com>
2022-09-25 15:58:30 +0700

    add Screen Navigation In React Native

---
+++ 4. React Native Functions and Usage/7. Screen Navigation In React Native/1. Types of Navigators and Uses of React Navigation/README.md
@@ -0,0 +1,9 @@
+# Types of Navigators and Uses of React Navigation
+
+Terdapat beberapa tipe navigasi yang bisa kita gunakan :
+
+1. Stack navigator. Stack navigator ini paling sering digunakan dalam aplikasi mobile, bentuk nya mirip seperti halaman web ia menumpuk satu layar dengan layar yang lainnya.
+
+2. Tab navigator. Tab navigator melakukan proses navigasi layar menggunakan navigasi tab.
+
+3. Drawer navigator yaitu navigasi dengan humberger menu.
---
+++ 4. React Native Functions and Usage/7. Screen Navigation In React Native/App.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react'
+import {
+  FlatList,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+} from 'react-native'
+import { createStackNavigator } from 'react-navigation'
+import Detail from './src/screens/Detail'
+
+class App extends Component {
+  state = {
+    podcasts: [
+      {
+        id: 1,
+        title: 'Ceritanya developer podcast',
+        url: 'https://ceritanya.developer.com',
+      },
+      {
+        id: 2,
+        title: 'DataPods',
+        url: 'https://datapods.com',
+      },
+      {
+        id: 3,
+        title: 'Wicara Podcast',
+        url: 'https://wicarapodcast.com',
+      },
+    ],
+
+    refreshing: false,
+  }
+
+  static navigationOptions = {
+    title: 'podspace',
+  }
+
+  renderItem = ({ item }) => (
+    <TouchableOpacity
+      style={{ padding: 15, borderBottomColor: '#ddd', borderBottomWidth: 1 }}
+      onPress={() => this.props.navigation.navigate('Detail')}
+    >
+      <Text style={styles.title}>{item.title}</Text>
+      <Text style={{ fontSize: 15 }}>{item.url}</Text>
+    </TouchableOpacity>
+  )
+
+  onRefresh = async () => {
+    this.setState({ refreshing: true }, () => {
+      const data = this.state.podcasts.concat({
+        id: 4,
+        title: 'New Podcast',
+        url: 'https://newpodcast.com',
+      })
+
+      this.setState({
+        podcasts: data,
+        refreshing: false,
+      })
+    })
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={this.state.podcasts}
+          keyExtractor={(item) => item.id}
+          renderItem={this.renderItem}
+          refreshing={this.state.refreshing}
+          onRefresh={this.onRefresh}
+        />
+      </SafeAreaView>
+    )
+  }
+}
+
+const Nav = createStackNavigator({
+  App: {
+    screen: App,
+  },
+  Detail: {
+    screen: Detail,
+  },
+})
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  title: {
+    color: '#000',
+    fontSize: 28,
+    fontWeight: '700',
+  },
+  span: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#1E1E1E',
+    fontSize: 20,
+    marginTop: 20,
+    fontWeight: '600',
+  },
+})
+
+export default Nav
